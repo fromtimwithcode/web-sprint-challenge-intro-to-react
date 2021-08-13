3858cd7c1f5474609f643bf44491cd266886434c
Tim Marchant <fromtimwithcode@gmail.com>
2021-08-13 13:13:49 -0500

    Set list of characters into stae and render characters to the DOM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Character from './components/Character'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   const [error, setError] = useState(null)
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
@@ -14,7 +15,6 @@ const App = () => {
   useEffect(() => {
     axios.get(`https://swapi.dev/api/people`)
       .then(res => {
-        console.log(res.data)
         setData(res.data)
       }).catch(err => {
         setError(`Please try again later.`)
@@ -24,6 +24,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character data={data} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,16 @@
 // Write your Character component here
+import React from 'react';
+
+export default function Character(props) {
+    const { data } = props;
+
+    return(
+        <div>
+            {data.map(dataObj => {
+                return(
+                    dataObj.name
+                )
+            })}
+        </div>
+    )
+}
